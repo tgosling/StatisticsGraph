337bf51ac7f8f1ef73fe2e946caffe1ee77ac21f
Tyler Gosling <t.gosling61@gmail.com>
2019-06-01 13:42:48 -0400

    ViewModel for MainWindow begun

---
+++ StatsGraph/ViewModel/ViewModelMain.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using StatsGraph.Helper;
+using StatsGraph.Model;
+using System.ComponentModel;
+
+
+namespace StatsGraph.ViewModel
+{
+    class ViewModelMain : ViewModelBase, INotifyPropertyChanged
+    {
+        public ObservableCollection<StatsModel> OCData { get; set; }
+        object _SelectedRow;
+        public object SelectedRow
+        {
+            get
+            {
+                return _SelectedRow;
+            }
+            set
+            {
+                _SelectedRow = value;
+                RaisePropertyChanged("SelectedRow");
+            }
+        }
+
+        //TODO: Remove Selected Row
+        private void RemoveRow(object obj)
+        {
+
+        }
+
+        public ViewModelMain()
+        {
+            OCData = new ObservableCollection<StatsModel>();
+        }
+    }
+}
