f37af5e9f72f23702ac0ec2facd525c215cf37ee
Tyler Gosling <t.gosling61@gmail.com>
2019-06-01 13:59:33 -0400

    Begun Handling populating mainwindow with Stats Data

--- StatsGraph/View/MainWindow.xaml.cs
+++ StatsGraph/View/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Microsoft.Win32;
+using StatsGraph.ViewModel;
+using StatsGraph.Helper;
+
 
 namespace StatsGraph
 {
@@ -20,39 +16,63 @@ namespace StatsGraph
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModelMain mainView { get; set; }
+        private string _currentFile;
         public MainWindow()
         {
             InitializeComponent();
+            mainView = new ViewModelMain();
+            DataContext = mainView;
+          
         }
-        /*
-                 <CommandBinding Command="ApplicationCommands.Open"
-                        Executed="CommandBinding_OpenExecuted"/>
-        <CommandBinding Command="ApplicationCommands.Close"
-                        Executed="CommandBinding_Close"/>
-        <CommandBinding Command="ApplicationCommands.Save"
-                        Executed="CommandBinding_SaveExecuted"/>
-        <CommandBinding Command="ApplicationCommands.New"
-                        Executed="CommandBinding_NewGraph"/>*/
+        /*  Method Name: CommandBinding_OpenExecuted 
+         *      Purpose: Handles the FileMenu->Open New File event
+         *      Accepts: object sender, ExecutedRoutedEventArgs e
+         *      Returns: void
+         */
         private void CommandBinding_OpenExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == true)
+            {
+                mainView.OCData.Clear();
+                foreach(var sm in FileParser.ReadCSVFile(ofd.FileName))
+                    mainView.OCData.Add(sm);
+            }
         }
-
+        /*  Method Name: CommandBinding_Close 
+         *      Purpose: Handles the FileMenu->Close/Exit event
+         *      Accepts: object sender, ExecutedRoutedEventArgs e
+         *      Returns: void
+         */
         private void CommandBinding_Close(object sender, ExecutedRoutedEventArgs e)
         {
             Close();
         }
-
+        /*  Method Name: CommandBinding_SaveExecuted 
+         *      Purpose: Handles the FileMenu->Save to File event
+         *      Accepts: object sender, ExecutedRoutedEventArgs e
+         *      Returns: void
+         */
         private void CommandBinding_SaveExecuted(object sender, ExecutedRoutedEventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == true)
+            {
+                _currentFile = sfd.FileName;
+                FileParser.WriteCSVFile(mainView.OCData, _currentFile);
+            }
         }
 
         private void CommandBinding_NewGraph(object sender, ExecutedRoutedEventArgs e)
         {
 
         }
-
+        /*  Method Name: HelpAbout_Click 
+         *      Purpose: Handles the HelpMenu->About event
+         *      Accepts: object sender, ExecutedRoutedEventArgs e
+         *      Returns: void
+         */
         private void HelpAbout_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Data Set: Population Estimates, Quarterly\n\n\nAuthor: Tyler MJ Gosling", "About");
